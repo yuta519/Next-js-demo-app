800cda696330a2a1a39fd05924dec7a5f283ba67
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-09-17 11:28:53 +0900

    create various files

--- components/pages/Copyright.tsx
+++ components/pages/Copyright.tsx
@@ -6,7 +6,7 @@ export function Copyright() {
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
         <Link color="inherit" href="https://material-ui.com/">
-          Your Website
+          dokudan
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
